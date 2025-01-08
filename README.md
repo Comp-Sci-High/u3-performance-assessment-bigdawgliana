700a49519ae9ac89f43d2d6e12b60ed19666b4fe
bigdawgliana <liana.ortiz25@compscihigh.org>
2025-01-08 20:08:14 +0000

    i finished yay

--- README.md
+++ README.md
@@ -1,3 +1,11 @@
-[![Review Assignment Due Date](https://classroom.github.com/assets/deadline-readme-button-22041afd0340ce965d47ae6ef1cefeee28c7c493a6346c4f15d667ab976d596c.svg)](https://classroom.github.com/a/HARs_Oy5)
-[![Open in Visual Studio Code](https://classroom.github.com/assets/open-in-vscode-2e0aaae1b6195c2367325f4f02e2d04e9abb55f0b24a779b69b11b9e10269abc.svg)](https://classroom.github.com/online_ide?assignment_repo_id=17629883&assignment_repo_type=AssignmentRepo)
-# node-template
+Why did you pick this API topic?
+
+I picked this topic because I'm in software engineering and I wanted to make a homeowrk helper for software engineering with vocab and smaple questions.
+
+Describe your API and the available data endpoints
+
+My API has two endpoints, /ap/vocab and /api/sample, this is to help support people with certain terms and then the sample questions are used to also test their knowledge on these topics, and to help if they dont udnertsnad.
+
+How was your experience building your own API server after using other APIs as a client in Unit 2?
+
+It was easy, I understand most of it, the basics
--- index.js
+++ index.js
@@ -1,1 +1,54 @@
-// Good luck!
+const express = require("express")
+const app = express()
+
+const homeworkHelper = {
+    vocab: [
+        { term: "API: Application Programming Interface" },
+        { term: "Modularization: Process of making code more compact and DRY"},
+        { term: "HTTP: Hypertext Text Transfer Protocol"},
+        { term: "Synchronous Programming: code that runs one line at a time top to bottom."},
+        { term: "Asynchronous Programming: code that allows us to start running multiple lines of code and finish them later and not in order."},
+    ],
+    sample: [
+        { ques: "What does API stand for?"},
+        { ques: "What does HTTP stand for?"},
+        { ques: "What does API stand for?"},
+        { ques: "What is modularization, and what is the purpose of it?"},
+        { ques: "What is the difference between asynchronous and synchronous programming?"},
+    ],
+};
+
+
+
+
+app.use((request, response, next ) => {
+    console.log(request.method + "" + request.url)
+    next()
+})
+
+app.get("/", (request, response) => {
+    response.status(200).send("<h1>Welcome to the Software Engineering Helper!")
+})
+
+
+app.get("/docs", (request, response) => {
+    response.status(200).send("Go to /api/vocab to see vocab terms and go to api/sample to see sample questions")
+})
+
+app.get("/api/vocab", (request, response) => {
+    response.status(200).json(homeworkHelper.vocab)
+})
+
+app.get("/api/sample", (request, response) => {
+    response.status(200).json(homeworkHelper.sample)
+})
+
+
+
+app.use((request, response, next) => {
+response.status(404).send("404 Not Found")
+})
+
+app.listen(3000, () => {
+    console.log("Server running")
+})
